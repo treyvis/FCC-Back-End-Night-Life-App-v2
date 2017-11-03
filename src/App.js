e1262f76db45f2f622d33ae37e84e9f44ce519da
Trey <treyvis@gmail.com>
2017-11-03 09:03:48 -0600

    Making List modular

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css'
-import { Menu, Icon, Card, Col} from 'antd';
 import { Layout } from 'antd';
 import burger from './img/burger.jpg';
 import Nav from './Components/Nav.js';
+import List from './Components/List.js';
 const { Sider, Content } = Layout;
 
 class App extends Component {
@@ -16,54 +16,7 @@ class App extends Component {
     return (
       <div className="App">
         <Nav />
-        <Card title='Burger 13' extra={<a href="#">More</a>} style={{ 
-          maxWidth: 800, 
-          width: '100%', 
-          margin: '10px auto',
-          }}>
-          <Layout>
-            <Sider>
-              <div className="custom-image">
-                <img alt="example" width="100%" src={burger} />
-              </div>
-            </Sider>
-            <Content style={{padding: 10}}>
-              <h2>American Grub with a Contempoary Twist</h2>
-            </Content>
-          </Layout>
-        </Card>
-        <Card title='Burger 13' extra={<a href="#">More</a>} style={{ 
-          maxWidth: 800, 
-          width: '100%', 
-          margin: '10px auto',
-          }}>
-          <Layout>
-            <Sider>
-              <div className="custom-image">
-                <img alt="example" width="100%" src={burger} />
-              </div>
-            </Sider>
-            <Content style={{padding: 10}}>
-              <h2>American Grub with a Contempoary Twist</h2>
-            </Content>
-          </Layout>
-        </Card>
-        <Card title='Burger 13' extra={<a href="#">More</a>} style={{ 
-          maxWidth: 800, 
-          width: '100%', 
-          margin: '10px auto',
-          }}>
-          <Layout>
-            <Sider>
-              <div className="custom-image">
-                <img alt="example" width="100%" src={burger} />
-              </div>
-            </Sider>
-            <Content style={{padding: 10}}>
-              <h2>American Grub with a Contempoary Twist</h2>
-            </Content>
-          </Layout>
-        </Card>
+        <List />
       </div>
     );
   }
---
+++ src/Components/List.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import burger from '../img/burger.jpg';
+import { Layout } from 'antd';
+import { Menu, Icon, Card, Col} from 'antd';
+const { Sider, Content } = Layout;
+
+class List extends Component {
+  render() {
+    return (
+      <div>
+        <Card title='Burger 13' extra={<a href="#">More</a>} style={{ 
+          maxWidth: 800, 
+          width: '100%', 
+          margin: '10px auto',
+          }}>
+          <Layout>
+            <Sider>
+              <div className="custom-image">
+                <img alt="example" width="100%" src={burger} />
+              </div>
+            </Sider>
+            <Content style={{padding: 10}}>
+              <h2>American Grub with a Contempoary Twist</h2>
+            </Content>
+          </Layout>
+        </Card>
+        <Card title='Burger 13' extra={<a href="#">More</a>} style={{ 
+          maxWidth: 800, 
+          width: '100%', 
+          margin: '10px auto',
+          }}>
+          <Layout>
+            <Sider>
+              <div className="custom-image">
+                <img alt="example" width="100%" src={burger} />
+              </div>
+            </Sider>
+            <Content style={{padding: 10}}>
+              <h2>American Grub with a Contempoary Twist</h2>
+            </Content>
+          </Layout>
+        </Card>
+        <Card title='Burger 13' extra={<a href="#">More</a>} style={{ 
+          maxWidth: 800, 
+          width: '100%', 
+          margin: '10px auto',
+          }}>
+          <Layout>
+            <Sider>
+              <div className="custom-image">
+                <img alt="example" width="100%" src={burger} />
+              </div>
+            </Sider>
+            <Content style={{padding: 10}}>
+              <h2>American Grub with a Contempoary Twist</h2>
+            </Content>
+          </Layout>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default List;
