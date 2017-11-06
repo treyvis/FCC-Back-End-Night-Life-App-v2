37c7f5e9bdc0616799fb1affeda15c657dc3f40a
Trey <treyvis@gmail.com>
2017-11-05 23:30:25 -0700

    fetching from yelp api

--- src/server/index.js
+++ src/server/index.js
@@ -5,3 +5,12 @@ const axios = require('axios');
 const config = require('./config.js');
 
 console.log(config);
+
+const yelp = axios.create({headers: {'Authorization':'Bearer ' + config.yelpToken}});
+
+yelp.get('https://api.yelp.com/v3/businesses/search?term=food&location=Las Vegas&limit=2&offset=2')
+  .then(res => {
+    console.log(res.data.businesses);
+  }).catch(err => {
+    console.error(error);
+  });
