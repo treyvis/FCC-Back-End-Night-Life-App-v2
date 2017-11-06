6778a7cea4562f5382b7e84f2fddf4d2b227fa9c
Trey <treyvis@gmail.com>
2017-11-06 09:05:14 -0700

    Handling search with routes on business search in yelp

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-
 const config = require('./config.js');
-
-console.log(config);
-
 const yelp = axios.create({headers: {'Authorization':'Bearer ' + config.yelpToken}});
 
-yelp.get('https://api.yelp.com/v3/businesses/search?term=food&location=Las Vegas&limit=2&offset=2')
-  .then(res => {
-    console.log(res.data.businesses);
+app.get('/api/:location', (req,res) => {
+  console.log('API pinged with', req.params.location);
+  yelp.get('https://api.yelp.com/v3/businesses/search?term=food&limit=2&location=' + req.params.location)
+  .then(data => {
+    res.json(data.data.businesses);
   }).catch(err => {
-    console.error(error);
-  });
+  console.error(err);
+  })
+});
+
+const port = 3001;
+
+app.listen(port, () => {
+  console.log('Listening on', port);
+});
