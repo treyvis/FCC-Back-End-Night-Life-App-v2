daaedd88a8a74affcbc5f7579f7794b1097c1769
Trey <treyvis@gmail.com>
2017-11-12 14:47:29 -0700

    rednering restaurnt list in list view

--- src/Components/List.js
+++ src/Components/List.js
@@ -5,9 +5,11 @@ const { Sider, Content } = Layout;
 
 class List extends Component {
   render() {
-    return (
-      <div>
-        <Card title='Burger 13' extra={<a href="#">More</a>} style={{ 
+    console.log('Restaurants',this.props.restaurants.length);
+
+    const restaurantCards = this.props.restaurants.map(restaurant => {
+      return (
+        <Card title={restaurant.name} key={restaurant.id} extra={<a href={restaurant.url} target='_blank'>See in Yelp</a>} style={{ 
           maxWidth: 800, 
           width: '100%', 
           margin: '10px auto',
@@ -15,50 +17,28 @@ class List extends Component {
           <Layout>
             <Sider >
               <div className="custom-image" style={{
-                background: 'url(https://s3-media1.fl.yelpcdn.com/bphoto/jmEG0-ADjgmRDYulOIXusg/o.jpg) center / cover no-repeat',
+                background: 'url(' + restaurant.image_url +') center / cover no-repeat',
                 width: '100%',
                 height: '200px'
               }}></div>
             </Sider>
             <Content style={{padding: 10}}>
-              <h2>American Grub with a Contempoary Twist</h2>
+              <h3>{restaurant.location.display_address[0]}</h3>
+              <h3>{restaurant.location.display_address[1]}</h3>
+              <h3>{restaurant.display_phone}</h3>
+              <br/>
+              <h3>{restaurant.price}</h3>
+              <br/>
+              <h3>{restaurant.rating}</h3>
             </Content>
           </Layout>
         </Card>
-        <Card title='Burger 13' extra={<a href="#">More</a>} style={{ 
-          maxWidth: 800, 
-          width: '100%', 
-          margin: '10px auto',
-          }}>
-          <Layout >
-            <Sider style={{height: '200px',width: '200px', overflow: 'hidden'}}>
-              <div className="custom-image" style={{
-                background: 'url(https://s3-media2.fl.yelpcdn.com/bphoto/AjNvDqZPZH8GoUe9GNLIAQ/o.jpg) center / cover no-repeat',
-                width: '200px',
-                height: '200px'
-              }}></div>
-            </Sider>
-            <Content style={{padding: 10}}>
-              <h2>American Grub with a Contempoary Twist</h2>
-            </Content>
-          </Layout>
-        </Card>
-        <Card title='Burger 13' extra={<a href="#">More</a>} style={{ 
-          maxWidth: 800, 
-          width: '100%', 
-          margin: '10px auto',
-          }}>
-          <Layout>
-            <Sider>
-              <div className="custom-image">
-                <img alt="example" width="100%" src={burger} />
-              </div>
-            </Sider>
-            <Content style={{padding: 10}}>
-              <h2>American Grub with a Contempoary Twist</h2>
-            </Content>
-          </Layout>
-        </Card>
+      );
+    });
+
+    return (
+      <div>
+        {restaurantCards}
       </div>
     );
   }
